ed3637deac1457d5ebc90e694ac4f2af9403a8a6
DoSangTao <hkngokhong@gmai.com>
2019-07-01 18:03:32 +0700

    first commit

---
+++ resources/js/layouts/master.vue
@@ -0,0 +1,74 @@
+<template>
+  <a-layout id="components-layout-demo-custom-trigger">
+    <a-layout-sider :trigger="null" collapsible v-model="collapsed">
+      <a-menu theme="dark" mode="inline" :defaultSelectedKeys="['1']" :defaultOpenKeys="['sub1']">
+        <a-menu-item key="1">
+          <a-icon type="pie-chart" />
+          <span>Option 1</span>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <a-icon type="desktop" />
+          <span>Option 2</span>
+        </a-menu-item>
+        <a-sub-menu key="sub1">
+          <span slot="title">
+            <a-icon type="user" />
+            <span>User</span>
+          </span>
+          <a-menu-item key="3">Tom</a-menu-item>
+          <a-menu-item key="4">Bill</a-menu-item>
+          <a-menu-item key="5">Alex</a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub2">
+          <span slot="title">
+            <a-icon type="team" />
+            <span>Team</span>
+          </span>
+          <a-menu-item key="6">Team 1</a-menu-item>
+          <a-menu-item key="8">Team 2</a-menu-item>
+        </a-sub-menu>
+        <a-menu-item key="9">
+          <a-icon type="file" />
+          <span>File</span>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header style="background: #fff; padding: 0">
+        <a-button
+          type="primary"
+          shape="circle"
+          style="margin-left: 20px; padding-bottom: 20px"
+          :icon="collapsed ? 'menu-unfold' : 'menu-fold'"
+          @click="()=> collapsed = !collapsed"
+        ></a-button>
+      </a-layout-header>
+      <a-layout-content
+        :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px' }"
+      >
+        <child />
+      </a-layout-content>
+    </a-layout>
+  </a-layout>
+</template>
+<script>
+export default {
+  name: "MasterLayout",
+
+  data() {
+    return {
+      collapsed: true
+    };
+  }
+};
+</script>
+
+<style>
+#components-layout-demo-top-side-2 .logo {
+  width: 120px;
+  height: 31px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px 28px 16px 0;
+  float: left;
+}
+</style>
---
+++ resources/js/pages/Excel/index.vue
@@ -0,0 +1,10 @@
+<template>
+	<div>
+		content master
+	</div>
+</template>
+<script>
+export default {
+	layout: 'master'
+}
+</script>
